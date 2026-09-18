3dcd98430b466569bccab054e3e5fa65c59a5841
Leonid Kuligin <lkulighin@gmail.com>
2026-09-18 10:53:43 +0000

    added inference.cli

---
+++ llm_monkeys/inference/cli.py
@@ -0,0 +1,12 @@
+from inference.run import async_main, build_config, create_parser, format_summary, main
+
+__all__ = [
+    "async_main",
+    "build_config",
+    "create_parser",
+    "format_summary",
+    "main",
+]
+
+if __name__ == "__main__":
+    main()
